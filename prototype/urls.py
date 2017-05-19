d6c6c0e3d426c1fa68f85b25d8604c4f60c899fb
gaurishankar11 <gshankarbade@gmail.com>
2017-05-19 17:59:51 +0530

    static

--- prototype/urls.py
+++ prototype/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, patterns
+from django.conf.urls import url
+from django.conf.urls.defaults import patterns
 from django.conf import settings
 
 from django.contrib import admin
