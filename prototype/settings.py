02aacb7ad29de24aa85bc423e99df1efd27c0ba4
gaurishankar11 <gshankarbade@gmail.com>
2017-05-19 16:55:14 +0530

    setting

--- prototype/settings.py
+++ prototype/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -138,3 +137,7 @@ EMAIL_USE_TLS = True
 BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
 ADMIN_SITE_HEADER = "My shiny new administration"
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
