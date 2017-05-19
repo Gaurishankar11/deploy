f5e5c0d0f280c06eddc01a29dc542fabd1dc583e
gaurishankar11 <gshankarbade@gmail.com>
2017-05-19 17:12:39 +0530

    static

--- prototype/settings.py
+++ prototype/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pbdi+mke+do*)4s3akn-rk@t6_y@-llr^^+2(^#l**n=#=a0-s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.afternoon-mountain-42194.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
