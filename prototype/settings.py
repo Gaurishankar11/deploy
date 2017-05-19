18260f5a67850489dfc259281741047fc4169397
gaurishankar11 <gshankarbade@gmail.com>
2017-05-19 17:19:34 +0530

    static

--- prototype/settings.py
+++ prototype/settings.py
@@ -141,3 +141,8 @@ ADMIN_SITE_HEADER = "My shiny new administration"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
